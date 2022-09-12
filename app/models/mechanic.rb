1f0a92e4c443a56e8a3fe97acdf7527a0c62152e
Lucas Colwell <colwellc37@gmail.com>
2022-09-12 10:36:31 -0600

    Update models for story 1

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,10 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+
+
+  def self.average_experience
+    mechanics = Mechanic.all
+    mechanics.average(:years_experience)
+  end
 end
