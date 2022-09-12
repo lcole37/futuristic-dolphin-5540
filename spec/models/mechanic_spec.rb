26baded18ea09a11894eacfc08cc9504141ac4df
Lucas Colwell <colwellc37@gmail.com>
2022-09-12 10:35:51 -0600

    Update model tests: story 1

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many(:ride_mechanics) }
     it { should have_many(:rides).through(:ride_mechanics) }
   end
+
+  describe 'methods' do
+    before :each do
+      @jimmy = Mechanic.create!(name: "Jimmy", years_experience: 2)
+      @mike = Mechanic.create!(name: "Mike", years_experience: 4)
+      @molly = Mechanic.create!(name: "Molly", years_experience: 6)
+    end
+
+    describe 'average_experience' do
+      it "returns average years of experience among mechanics" do
+        @mechanics = Mechanic.all
+        expect(@mechanics.average_experience).to eq(4)
+
+        @Jane = Mechanic.create!(name: "Jane", years_experience: 8)
+        @mechanics = Mechanic.all
+
+        expect(@mechanics.average_experience).to eq(5)
+      end
+    end
+  end
 end
