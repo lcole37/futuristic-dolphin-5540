4eed060cb4f561efb165a8300a90a7c6d0a8c099
Lucas Colwell <colwellc37@gmail.com>
2022-09-12 09:20:38 -0600

    Test: story 1 tests

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user,' do
+  describe 'When I visit the mechanics index page' do
+    before :each do
+      @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+      @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+
+      @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+      @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+      @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+      @jimmy = Mechanic.create!(name: "Jimmy", years_experience: 2)
+      @mike = Mechanic.create!(name: "Mike", years_experience: 4)
+      @molly = Mechanic.create!(name: "Molly", years_experience: 6)
+
+      visit "/mechanics" #I put visit in the "before" as this describe says "when I visit ___"
+    end
+
+    it 'I see a header saying “All Mechanics”' do
+      expect(page).to have_content("All Mechanics")
+    end
+
+    it 'And I see a list of all mechanic’s names and their years of experience' do
+      #within blocks could be used, but time
+      expect(page).to have_content(@jimmy.name)
+      expect(page).to have_content(@mike.name)
+      expect(page).to have_content(@molly.name)
+      expect(page).to have_content(@jimmy.years_experience)
+      expect(page).to have_content(@mike.years_experience)
+      expect(page).to have_content(@molly.years_experience)
+    end
+
+    it 'And I see the average years of experience across all mechanics' do
+      expect(page).to have_content("Average Years of Esperience: 4")
+    end
+  end
+end
