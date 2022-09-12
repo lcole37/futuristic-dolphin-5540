a33c4ced83c4d78e0ea630356d887efd19bae6e4
Lucas Colwell <colwellc37@gmail.com>
2022-09-12 09:49:58 -0600

    Test: update string typo

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe 'As a user,' do
 
     it 'I see a header saying “All Mechanics”' do
       expect(page).to have_content("All Mechanics")
+      #is there a way to test that it is header/first on page?
     end
 
     it 'And I see a list of all mechanic’s names and their years of experience' do
@@ -28,13 +29,13 @@ RSpec.describe 'As a user,' do
       expect(page).to have_content(@jimmy.name)
       expect(page).to have_content(@mike.name)
       expect(page).to have_content(@molly.name)
-      expect(page).to have_content(@jimmy.years_experience)
-      expect(page).to have_content(@mike.years_experience)
-      expect(page).to have_content(@molly.years_experience)
+      expect(page).to have_content("Years of Experience: #{@jimmy.years_experience}")
+      expect(page).to have_content("Years of Experience: #{@mike.years_experience}")
+      expect(page).to have_content("Years of Experience: #{@molly.years_experience}")
     end
 
     it 'And I see the average years of experience across all mechanics' do
-      expect(page).to have_content("Average Years of Esperience: 4")
+      expect(page).to have_content("Average Years of Experience: 4")
     end
   end
 end
